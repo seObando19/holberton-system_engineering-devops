0c427020c8ff240cb602d5a840b903a75bc8ae05
seObando19 <sebastiano-10@hotmail.com>
2021-02-01 19:14:33 -0500

    Completed v2 task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                          .format(URL, employee_id)).json()
     NUMBER_OF_DONE_TASKS = len(completed_task)
 
-    print("Employee {} is done with tasks({}/{})"
+    print("Employee {} is done with tasks({}/{}):"
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
     for task in completed_task:
