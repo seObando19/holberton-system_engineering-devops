1effb95dfab8c755248ca96621704eb101ac38a6
seObando19 <sebastiano-10@hotmail.com>
2021-02-01 19:28:23 -0500

    Completed v4 task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,17 +12,13 @@ if __name__ == "__main__":
 
     employee_id = argv[1]
     URL = "https://jsonplaceholder.typicode.com/"
-
     user = get('{}users/{}'.format(URL, employee_id)).json()
     EMPLOYEE_NAME = user['name']
-
     todo = get('{}todos?userId={}'.format(URL, employee_id)).json()
     TOTAL_NUMBER_OF_TASKS = len(todo)
-
     completed_task = get('{}todos?userId={}&&completed=true'
                          .format(URL, employee_id)).json()
     NUMBER_OF_DONE_TASKS = len(completed_task)
-
     print("Employee {} is done with tasks({}/{}):"
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
