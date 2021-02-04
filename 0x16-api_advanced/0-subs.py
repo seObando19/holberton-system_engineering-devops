9063e48d5f42e6213e32c53c117891dc72701a0b
seObando19 <sebastiano-10@hotmail.com>
2021-02-04 15:32:46 -0500

    Completed task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+
+"""
+    function that queries the Reddit API and returns the number
+    of subscribers (not active users, total subscribers) for a
+    given subreddit. If an invalid subreddit is given, the function
+    should return 0
+"""
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns
+    the number of subscribers for a given subreddit"""
+
+    URL = 'https://www.reddit.com/'
+    path = 'r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'API advance Sebas'}
+
+    res = requests.get("{}{}".format(URL, path), headers=header)
+
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    else:
+        return 0
